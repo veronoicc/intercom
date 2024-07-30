8e1d9fd93e1c34cba6d3beedecc55e625474e3db
veronoicc <64193056+veronoicc@users.noreply.github.com>
2024-07-30 21:09:22 +0000

    add lib

---
+++ src/lib.rs
@@ -0,0 +1,124 @@
+#[cfg(not(any(feature = "ring", feature = "aws-lc-rs")))]
+compile_error!("Either the ring or aws-lc-rs feature must be enabled");
+
+#[cfg(not(any(feature = "json", feature = "msgpack")))]
+compile_error!("Either the json or msgpack feature must be enabled");
+
+use std::marker::PhantomData;
+
+pub use async_nats as nats;
+use nats::{subject::ToSubject, ToServerAddrs};
+use serde::{Deserialize, Serialize};
+
+pub mod subscriber;
+
+pub trait Encoder {}
+
+#[cfg(feature = "json")]
+pub struct JsonEncoder;
+#[cfg(feature = "json")]
+impl Encoder for JsonEncoder {}
+
+#[cfg(feature = "msgpack")]
+pub struct MessagePackEncoder;
+#[cfg(feature = "msgpack")]
+impl Encoder for MessagePackEncoder {}
+
+pub struct JetStream;
+
+#[derive(Clone, Copy, Debug)]
+pub(crate) enum Encoding {
+    NoEncoding,
+    #[cfg(feature = "json")]
+    Json,
+    #[cfg(feature = "msgpack")]
+    MessagePack,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Message<T> {
+    /// Subject to which message is published to.
+    pub subject: nats::Subject,
+    /// Optional reply subject to which response can be published by [crate::Subscriber].
+    /// Used for request-response pattern with [crate::Client::request].
+    pub reply: Option<nats::Subject>,
+    /// Payload of the message. Can be any arbitrary data format.
+    pub payload: T,
+    /// Optional headers.
+    pub headers: Option<nats::HeaderMap>,
+    /// Optional Status of the message. Used mostly for internal handling.
+    pub status: Option<nats::StatusCode>,
+    /// Optional [status][crate::Message::status] description.
+    pub description: Option<String>,
+
+    pub length: usize,
+}
+
+pub struct Intercom<E> {
+    inner: nats::Client,
+    encoding: Encoding,
+    _phantom: PhantomData<E>,
+}
+
+impl From<nats::Client> for Intercom<()> {
+    fn from(inner: nats::Client) -> Self {
+        Self {
+            inner,
+            encoding: Encoding::NoEncoding,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl Intercom<()> {
+    pub async fn connect<A: ToServerAddrs>(uri: A) -> Result<Self, async_nats::Error> {
+        let inner = nats::connect(uri).await?;
+        Ok(Self::from(inner))
+    }
+
+    pub async fn connect_with_options<A: ToServerAddrs>(
+        uri: A,
+        options: nats::ConnectOptions,
+    ) -> Result<Self, async_nats::Error> {
+        let inner = nats::connect_with_options(uri, options).await?;
+        Ok(Self::from(inner))
+    }
+
+    #[cfg(feature = "json")]
+    pub fn json(self) -> Intercom<JsonEncoder> {
+        Intercom {
+            inner: self.inner,
+            encoding: Encoding::Json,
+            _phantom: PhantomData,
+        }
+    }
+
+    #[cfg(feature = "msgpack")]
+    pub fn message_pack(self) -> Intercom<MessagePackEncoder> {
+        Intercom {
+            inner: self.inner,
+            encoding: Encoding::MessagePack,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<E: Encoder> Intercom<E> {
+    pub async fn publish<S: ToSubject, T: Serialize>(
+        &self,
+        subject: S,
+        payload: T,
+    ) -> Result<(), async_nats::Error> {
+        let payload = match self.encoding {
+            Encoding::NoEncoding => unreachable!(),
+            #[cfg(feature = "json")]
+            Encoding::Json => serde_json::to_vec(&payload)?,
+            #[cfg(feature = "msgpack")]
+            Encoding::MessagePack => rmp_serde::to_vec(&payload)?,
+        };
+
+        self.inner.publish(subject, payload.into()).await?;
+
+        Ok(())
+    }
+}
---
+++ src/subscriber.rs
@@ -0,0 +1,107 @@
+use std::{
+    marker::PhantomData,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use async_nats::subject::ToSubject;
+use futures::Stream;
+use pin_project_lite::pin_project;
+use serde::de::DeserializeOwned;
+use thiserror::Error;
+
+use crate::{nats, Encoder, Encoding, Intercom, Message};
+
+#[derive(Debug, Error)]
+pub enum SubscriberError {
+    #[error("NATS error: {0}")]
+    Nats(nats::Error),
+    #[cfg(feature = "json")]
+    #[error("JSON error: {0}")]
+    Json(serde_json::Error),
+    #[cfg(feature = "msgpack")]
+    #[error("MessagePack error: {0}")]
+    MessagePack(rmp_serde::decode::Error),
+}
+
+pin_project! {
+    pub struct Subscriber<T: DeserializeOwned> {
+        #[pin]
+        inner: nats::Subscriber,
+        encoding: Encoding,
+        has_closed: bool,
+        _phantom: PhantomData<T>,
+    }
+}
+
+impl<T: DeserializeOwned> Stream for Subscriber<T> {
+    type Item = Result<Message<T>, SubscriberError>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+
+        loop {
+            if *this.has_closed {
+                return Poll::Ready(None);
+            }
+
+            match Pin::new(&mut this.inner).poll_next(cx) {
+                Poll::Ready(Some(msg)) => {
+                    let payload = match this.encoding {
+                        Encoding::NoEncoding => unreachable!(),
+                        #[cfg(feature = "json")]
+                        Encoding::Json => serde_json::from_slice(&msg.payload)
+                            .map_err(|err| SubscriberError::Json(err)),
+                        #[cfg(feature = "msgpack")]
+                        Encoding::MessagePack => rmp_serde::from_slice(&msg.payload)
+                            .map_err(|err| SubscriberError::MessagePack(err)),
+                    };
+
+                    return Poll::Ready(Some(payload.map(|payload| Message {
+                        subject: msg.subject,
+                        reply: msg.reply,
+                        payload,
+                        headers: msg.headers,
+                        status: msg.status,
+                        description: msg.description,
+                        length: msg.length,
+                    })));
+                }
+                Poll::Ready(None) => return Poll::Ready(None),
+                Poll::Pending => {}
+            }
+        }
+    }
+}
+
+impl<E: Encoder> Intercom<E> {
+    pub async fn subscribe<S: ToSubject>(
+        &self,
+        subject: S,
+    ) -> Result<Subscriber<()>, async_nats::Error> {
+        let inner = self.inner.subscribe(subject).await?;
+        Ok(Subscriber {
+            inner,
+            encoding: self.encoding,
+            has_closed: false,
+            _phantom: PhantomData,
+        })
+    }
+
+    pub async fn queue_subscribe<S: ToSubject, Q: ToString>(
+        &self,
+        subject: S,
+        group: Q,
+    ) -> Result<Subscriber<()>, async_nats::Error> {
+        let inner = self
+            .inner
+            .queue_subscribe(subject, group.to_string())
+            .await?;
+        Ok(Subscriber {
+            inner,
+            encoding: self.encoding,
+            has_closed: false,
+            _phantom: PhantomData,
+        })
+    }
+}
